a596c7b5764db93edcbe435fa097243993092b61
pranesh7-18 <praneshyou@gmail.com>
2025-03-26 16:08:37 +0530

    updated profile.controller.js

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -3,23 +3,27 @@ import Tweet from "../models/tweet.js";
 import bcrypt from "bcrypt";
 import generateToken from "../utils/generateToken.js";
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: true, // Required for HTTPS
+  sameSite: "None", // Needed for cross-origin cookies
+  domain: "onrender.com", // Ensure cookie is available across backend
+};
+
 // SIGN UP
 export const signUp = async (req, res) => {
   const { username, email } = req.body;
-  const user = await Profile.findOne({
-    $or: [{ email: email }, { username: username }],
-  });
-  if (user)
-    return res
-      .status(401)
-      .json({ errors: [{ msg: "This user is already exist" }] });
+  const user = await Profile.findOne({ $or: [{ email }, { username }] });
+
+  if (user) {
+    return res.status(401).json({ errors: [{ msg: "This user already exists" }] });
+  }
 
   const profile = await Profile.create(req.body);
-  const token = profile && generateToken(profile._id);
+  const token = generateToken(profile._id);
+
   return res
-    .cookie("access_token", token, {
-      httpOnly: true,
-    })
+    .cookie("access_token", token, cookieOptions)
     .status(200)
     .json(profile);
 };
@@ -28,33 +32,29 @@ export const signUp = async (req, res) => {
 export const signIn = async (req, res) => {
   const { username, password } = req.body;
 
-  const profile = await Profile.findOne({
-    $or: [{ username: username }, { email: username }],
-  });
-  const bcryptedPasword =
-    profile && (await bcrypt.compare(password, profile.password));
+  const profile = await Profile.findOne({ $or: [{ username }, { email: username }] });
+  const isPasswordValid = profile && (await bcrypt.compare(password, profile.password));
 
-  if (bcryptedPasword && profile) {
+  if (isPasswordValid && profile) {
     const token = generateToken(profile._id);
     await profile.signIn();
-    res
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
+
+    return res
+      .cookie("access_token", token, cookieOptions)
       .status(200)
       .json(profile);
   } else {
-    res.status(401).json({ errors: [{ msg: "Invalid credentials" }] });
+    return res.status(401).json({ errors: [{ msg: "Invalid credentials" }] });
   }
 };
 
+// SIGN OUT
 export const signOut = async (req, res) => {
   const profile = req.user;
   await profile.signOut();
+
   return res
-    .cookie("access_token", "", {
-      httpOnly: true,
-    })
+    .cookie("access_token", "", { ...cookieOptions, maxAge: 0 }) // Clear the cookie
     .status(200)
     .json({ msg: "Successfully signed out." });
 };
@@ -64,17 +64,19 @@ export const follow = async (req, res) => {
   const { username } = req.query;
   const user = req.user;
 
-  const profile = await Profile.findOne({ username: username });
+  const profile = await Profile.findOne({ username });
   await user.follow(profile);
   res.send(profile);
 };
 
-//UNFOLLOW PROFILE
+// UNFOLLOW PROFILE
 export const unfollow = async (req, res) => {
   const { username } = req.query;
   const user = req.user;
-  const profile = await Profile.findOne({ username: username });
+
+  const profile = await Profile.findOne({ username });
   await user.unfollow(profile);
+
   return res.json(profile);
 };
 
@@ -82,20 +84,16 @@ export const unfollow = async (req, res) => {
 export const getProfile = async (req, res) => {
   const { username } = req.query;
   if (username) {
-    const profile = await Profile.findOne(
-      { username: username },
-      { password: 0 }
-    );
-    res.send(profile);
+    const profile = await Profile.findOne({ username }, { password: 0 });
+    return res.send(profile);
   } else {
-    const profile = await Profile.find({}, { password: 0 });
-    res.send(profile);
+    const profiles = await Profile.find({}, { password: 0 });
+    return res.send(profiles);
   }
 };
 
 export const getCurrentProfile = (req, res) => {
-  const profile = req.user;
-  res.send(profile);
+  res.send(req.user);
 };
 
 // BOOKMARKS
@@ -103,6 +101,7 @@ export const addBookmark = async (req, res) => {
   const { id } = req.query;
   const profile = req.user;
   const tweet = await Tweet.findOne({ _id: id });
+
   await profile.addBookmark(tweet);
   res.send(profile);
 };
@@ -122,11 +121,12 @@ export const removeBookmark = async (req, res) => {
   const { id } = req.query;
   const profile = req.user;
   const tweet = await Tweet.findOne({ _id: id });
+
   await profile.removeBookmark(tweet);
   res.send(profile);
 };
 
-// EDIT PROFILE BIO
+// EDIT PROFILE
 export const uploadAvatar = async (req, res) => {
   const { user, avatar } = req;
   await user.changeAvatar(avatar);
